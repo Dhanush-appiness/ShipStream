b8870e214407739193643132e730af8c2c92947b
Appiness R <appinessr@Appinesss-MacBook-Pro.local>
2026-08-19 17:36:41 +0530

    Update development email configuration

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,7 @@
+import os
+
 from .base import *  # noqa: F403
+
 
 DEBUG=True
 
@@ -7,6 +10,13 @@ ALLOWED_HOSTS=[
     '127.0.0.1',
 ]
 
-EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL='noreply@shipstream.local'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_PORT=int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+
+DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL')
 FRONTEND_URL='http://localhost:3000'
